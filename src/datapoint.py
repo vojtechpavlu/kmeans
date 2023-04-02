60d8497ea94637a3997bcb83de3c0d774475917e
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-04-02 16:46:56 +0200

    Add `variance` property to the `Cluster` class

--- src/datapoint.py
+++ src/datapoint.py
@@ -5,6 +5,8 @@ datapoints in the multidimensional space.
 from typing import Iterable, Dict
 from copy import deepcopy
 
+from src.metric import Metric
+
 
 class Point:
     """Instances of this class represents the points in the multidimensional
@@ -145,6 +147,16 @@ class Centroid(Point):
         """Calculates the two framing points describing the space the
         datapoints being assigned to this cluster (centroid)."""
         return frame_of(self.points)
+
+    def variance(self, metric: Metric) -> float:
+        """Calculates the variance of the cluster by squaring the distances
+        between the current centroid coordinates and each of the point
+        assigned to it.
+
+        To achieve this approach, the metric to calculate the distance has to
+        be provided."""
+        dist_squares = [metric.distance(self, p) ** 2 for p in self.points]
+        return sum(dist_squares)
 
     def __repr__(self):
         """String representation of the cluster."""
